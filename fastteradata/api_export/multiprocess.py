f937ce40d7fe2c97693e86e95a13bc08eb39b4d7
Poseidonx <Marty.jb.54321@gmail.com>
2018-01-22 14:03:53 -0600

    Update multiprocess.py

--- fastteradata/api_export/multiprocess.py
+++ fastteradata/api_export/multiprocess.py
@@ -6,6 +6,6 @@ def call_sub(f):
     print(f"Calling Fast Export on file...  {f}")
     sys.stdout.flush()
     p1= subprocess.Popen(f"fexp < {f}", shell=True, stdout=subprocess.PIPE)
-    print(p1.communicate()[0])
+    print(p1.stdout)
     sys.stdout.flush()
     return("")
